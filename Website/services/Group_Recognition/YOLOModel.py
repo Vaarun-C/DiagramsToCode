fa573d8340874ca4b10f13420a4c6ce111d35c5f
RockerBot <rockerbotmail@gmail.com>
2024-11-28 20:47:17 +0530

    + group_recognition service

---
+++ Website/services/Group_Recognition/YOLOModel.py
@@ -0,0 +1,34 @@
+from ultralytics import YOLO
+from PIL import Image
+from states import detection_object
+from ultralytics.engine.results import Results
+
+class yolomodel:
+    def __init__(self) -> None:       
+        self.model = YOLO("3_categories_best.pt")
+        self.conf_thresh = 0.8
+        self.CLS_NAME_TO_TYPE = {
+            "Private-subnet_32": "Private_Subnet",
+            "Public-subnet_32": "Public_Subnet",
+            "Virtual-private-cloud-VPC_32": "VPC"
+        }
+        self.model_names = self.model.names
+
+    def predict(self, architectureDiagram: Image) -> list[detection_object]:
+        all_classes = set(range(3))
+
+        results = self.model.predict(architectureDiagram, save=False, classes=all_classes, conf=self.conf_thresh)
+        detections = results[0].boxes
+        detection_objects = []
+        
+        for det in detections:
+            x_min, y_min, x_max, y_max = det.xyxy[0].tolist()
+            class_name = self.model_names[int(det.cls)]
+
+            try:
+                class_type = self.CLS_NAME_TO_TYPE[class_name]
+            except KeyError:
+                print(class_name, "Woah, Doesn't seem to be in list")
+
+            detection_objects.append(detection_object([x_max, y_max, x_min, y_min], class_type, float(det.conf)))
+        return detection_objects
---
+++ Website/services/Group_Recognition/states.py
@@ -0,0 +1,21 @@
+class detection_object:
+    def __init__(self, detection_box=[], class_type: str = None, confidence: float = 0.00) -> None:
+        self.box = detection_box
+        self.classType = class_type
+        self.confidenceValue = confidence
+
+    def to_dict(self):
+        return {"pos": self.box, "classType": self.classType, "confidence": self.confidenceValue}
+
+class Group:
+    def __init__(self, x,y,w,h,group_type=None) -> None:
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.group_type = group_type
+    
+    def __repr__(self):
+        if self.group_type is None:
+            return f'Group[{(self.x,self.y)=}, {self.w=}, {self.h=}]'
+        return f'Group[{self.group_type}][{(self.x,self.y)=}, {self.w=}, {self.h=}]'
